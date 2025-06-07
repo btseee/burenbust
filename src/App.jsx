2cb490751c6c320bc3779e6a17235943f0430ea0
Battseren Badral <bbattseren88@gmail.com>
2025-06-07 19:11:46 +0200

    fix: update image lightbox import to use react-18-image-lightbox

--- src/App.jsx
+++ src/App.jsx
@@ -7,7 +7,7 @@ import {
   useNavigationType,
 } from "react-router-dom";
 import "react-toastify/dist/ReactToastify.css";
-import "react-image-lightbox/style.css";
+import "react-18-image-lightbox/style.css";
 import "./App.css";
 
 import HomeOne from "./pages/HomeOne";
